21716cef4ee54008f46331ce0a36791262e5fe1d
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-07 11:23:25 +0530

    added asyn cal with dispacth in action generator now acting as a middleware

--- src/redux/actions/usersAction.generator.js
+++ src/redux/actions/usersAction.generator.js
@@ -1,9 +1,21 @@
 import { userActionTypes } from "../constants/usersAction.types";
 
+const url = "https://jsonplaceholder.typicode.com/users";
+
 const usersActionGenerator = (actionType, payload = []) => {
   switch (actionType) {
     case userActionTypes.LOAD:
       return { type: userActionTypes.LOAD, payload: { users: [...payload] } };
+
+    case userActionTypes.THUNK_LOAD:
+      return async (dispatch) => {
+        let response = await fetch(url);
+        let data = await response.json();
+        dispatch({
+          type: userActionTypes.THUNK_LOAD,
+          payload: { users: [...data] },
+        });
+      };
 
     default:
       return {
