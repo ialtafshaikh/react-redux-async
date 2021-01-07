57d827ed1f15facaa2578d7dff64f7634be088b2
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-07 11:24:40 +0530

    combine the reducers

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users.reducer";
+import usersThunkReducer from "./users.thunk.reducer";
 
 const rootReducer = combineReducers({
   usersReducer,
+  usersThunkReducer,
 });
 
 export default rootReducer;
